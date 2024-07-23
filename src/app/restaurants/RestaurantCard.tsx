7752fb15a4a2cb22350a90b77f003338d97903d3
B3Kay <benjaminjekarlsson@gmail.com>
2024-07-23 18:29:03 +0200

    Add restaurant page with boiler data

---
+++ src/app/restaurants/RestaurantCard.tsx
@@ -0,0 +1,70 @@
+import { formatCurrency } from "@/utils/formatCurrency";
+import Link from "next/link";
+import { Restaurant } from "./types";
+
+
+interface RestaurantRating {
+    rating: number;
+    roundedRating: number;
+    id: string;
+}
+
+export const RestaurantRating = ({ rating, roundedRating, id }: RestaurantRating) => {
+    return <div className=' flex items-baseline gap-2'>
+        <p className='m-0 max-w-[30ch] text-2xl font-black '>
+            {rating}
+        </p>
+        <div className="rating">
+            {[...Array(5)].map((_, index) => {
+                const isChecked = (index + 1) === roundedRating;
+                return (
+                    <input
+                        key={`${index}_${id}`}
+                        type="radio"
+                        name={`rating_${index}_${id}`}
+                        className="mask mask-star bg-red-400"
+                        checked={isChecked}
+                        readOnly />
+                );
+            })}
+        </div>
+    </div>;
+}
+
+
+export function RestaurantCard(restaurant: Restaurant) {
+
+    const rating = restaurant?.rating ?? 0
+    const roundedRating = Math.round(rating);
+    return (
+        <Link href={`/restaurants/${restaurant.id}`} >
+
+            <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+                <img src={restaurant.image_url} alt={`${restaurant.name} image`} className="w-full h-48 object-cover object-center rounded-lg" />
+                <p className="mt-3 max-w-[30ch] text-sm opacity-50">
+                    {restaurant.address}
+                </p>
+                <h2 className="mb-2 text-2xl font-semibold">
+                    {restaurant.name}{" "}
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                        -&gt;
+                    </span>
+                </h2>
+
+
+                <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                    {restaurant.description}
+                </p>
+
+                <p className='mt-5 text-sm font-bold opacity-50'>{formatCurrency(restaurant.price!!, 'kronor')}</p>
+                <div className='flex justify-between  gap-2'>
+
+
+                    <RestaurantRating rating={rating} roundedRating={roundedRating} id={restaurant.id!!} />
+
+                </div>
+
+            </div>
+        </Link >
+    );
+}
--- src/app/restaurants/[id]/page.tsx
+++ src/app/restaurants/[id]/page.tsx
@@ -1,11 +1,9 @@
 import PocketBase from 'pocketbase';
+import { RestaurantRating } from '../RestaurantCard';
+import { Restaurant } from '../types';
+import { formatCurrency } from '@/utils/formatCurrency';
 
-type Restaurant = {
-    id: string;
-    name: string;
-    description: string;
-    adress: string;
-}
+export const revalidate = 1
 
 async function getRestaurant(restaurantId: string) {
     const db = new PocketBase('http://127.0.0.1:8090');
@@ -14,16 +12,61 @@ async function getRestaurant(restaurantId: string) {
     return data;
 }
 
+const ImageGrid = ({ images: imageUrls }: { images: string[] }) => {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {imageUrls.map((image) => (
+                <img src={image} alt={` image`} className="w-full h-48 object-cover object-center rounded-lg" />
+            ))}
+        </div>
+    );
+}
+
 export default async function RestaurantPage({ params }: { params: { id: string } }) {
     const restaurant = await getRestaurant(params.id);
 
+
+    const placeholderImageUrls = [
+        'https://images.unsplash.com/photo-1605321995625-c77c7204cd70?q=80&w=1635&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1533142146849-4620b8191531?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1524932326868-56e1f1ede465?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1687254351560-b8fb47e2bdff?q=80&w=1487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D']
+
     return (
         <div className="flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold">Restaurant / {restaurant.id}</h1>
-            <div className="bg-slate-800 shadow-md rounded-lg p-4 m-2">
-                <h2 className="text-xl font-bold">{restaurant.name}</h2>
-                <p className="text-gray-300">{restaurant.description}</p>
-                <p className="text-gray-300">{restaurant.adress}</p>
+            <h1 className="mb-12 text-3xl font-bold">Restaurant / {restaurant.id}</h1>
+
+            <div >
+                <ImageGrid images={placeholderImageUrls} />
+                <h1 className='mb-1 mt-3 text-3xl font-bold'>{restaurant.name}</h1>
+                <div className="badge badge-outline badge-primary">{restaurant.type}</div>
+                <p className='mt-5 text-sm font-bold opacity-50'>{formatCurrency(restaurant.price!!, 'kronor')}</p>
+                <RestaurantRating rating={restaurant.rating!!} roundedRating={Math.round(restaurant.rating!!)} id={restaurant.id!!} />
+
+                <div className="divider"></div>
+                <div className='flex gap-3'>
+
+                    <a href={restaurant.website} target="_blank" rel="noreferrer">
+                        <button className="btn btn-sm btn-outline rounded-full">Website</button>
+                    </a>
+                    <a href={restaurant.website} target="_blank" rel="noreferrer">
+                        <button className="btn btn-sm btn-outline rounded-full">Directions</button>
+                    </a>
+                    <a href={restaurant.website} target="_blank" rel="noreferrer">
+                        <button className="btn btn-sm btn-outline rounded-full">Save</button>
+                    </a>
+                </div>
+                <div className="divider"></div>
+                <div className='flex flex-col gap-3'>
+                    <h4 className=' opacity-50'>{restaurant.address}</h4>
+                    <h4 className=' opacity-50'>031-312 76 77</h4>
+                    <h4 className=''><span className='opacity-100 text-success'>Open</span> - Closes 20:00</h4>
+                </div>
+                <div className="divider"></div>
+                <h3 className='text-lg mb-2 font-bold'>Description</h3>
+                <p className="m-0 max-w-[30ch] text-base opacity-50">
+                    {restaurant.description}
+                </p>
             </div>
         </div>
     );
