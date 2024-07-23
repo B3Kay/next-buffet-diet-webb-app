2d1871a7ca94eb74e4796b2cdd7616e3a48ff03c
B3Kay <benjaminjekarlsson@gmail.com>
2024-07-23 21:26:28 +0200

    Show only one image in restaurant view

--- src/app/restaurants/[id]/page.tsx
+++ src/app/restaurants/[id]/page.tsx
@@ -34,12 +34,16 @@ export default async function RestaurantPage({ params }: { params: { id: string 
         'https://images.unsplash.com/photo-1524932326868-56e1f1ede465?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         'https://images.unsplash.com/photo-1687254351560-b8fb47e2bdff?q=80&w=1487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D']
 
+    const images = [restaurant.image_url!!]
     return (
         <div className="">
             <h1 className="mb-12 mt-2 text-3xl font-bold">Restaurant / {restaurant.id}</h1>
+            {images.length > 1 ?
+                <ImageGrid images={images} />
+                :
+                <img src={restaurant.image_url} alt={`${restaurant.name} image`} className="w-full max-h-96 object-cover object-center rounded-3xl" />
+            }
 
-
-            <ImageGrid images={placeholderImageUrls} />
             <h1 className='mb-1 mt-5 text-3xl font-bold'>{restaurant.name}</h1>
             <div className="badge badge-outline badge-primary">{restaurant.type}</div>
             <p className='mt-5 text-sm font-bold opacity-50'>{formatCurrency(restaurant.price!!, 'kronor')}</p>
@@ -49,13 +53,13 @@ export default async function RestaurantPage({ params }: { params: { id: string 
             <div className='flex gap-3'>
 
                 <a href={restaurant.website} target="_blank" rel="noreferrer">
-                    <button className="btn btn-sm btn-outline rounded-full">Website</button>
+                    <button className="btn btn-sm btn-outline rounded-full"><Icon icon="lucide:globe" />Website</button>
                 </a>
-                <a href={restaurant.website} target="_blank" rel="noreferrer">
-                    <button className="btn btn-sm btn-outline rounded-full">Directions</button>
+                <a href={`https://www.google.com/maps/dir/?api=1&destination=${restaurant.website}`} target="_blank" rel="noreferrer">
+                    <button className="btn btn-sm btn-outline rounded-full"><Icon icon="lucide:signpost" />Directions</button>
                 </a>
-                <a href={restaurant.website} target="_blank" rel="noreferrer">
-                    <button className="btn btn-sm btn-outline rounded-full">Save</button>
+                <a href="#" rel="noreferrer">
+                    <button className="btn btn-sm btn-outline rounded-full"><Icon icon="lucide:save" />Save</button>
                 </a>
             </div>
             <div className="divider"></div>
