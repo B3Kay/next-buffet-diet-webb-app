501fb8853e8ea5beaa24c0806d47e04235bcbd4b
B3Kay <benjaminjekarlsson@gmail.com>
2024-09-19 11:57:23 +0200

    Add longitudes and latitudes to restaurants in DB

---
+++ pb_migrations/1726571662_updated_restaurants.js
@@ -0,0 +1,50 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("h69hbgnivso9umu")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "npblgzmz",
+    "name": "longitude",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "nughv3mw",
+    "name": "latitude",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("h69hbgnivso9umu")
+
+  // remove
+  collection.schema.removeField("npblgzmz")
+
+  // remove
+  collection.schema.removeField("nughv3mw")
+
+  return dao.saveCollection(collection)
+})
