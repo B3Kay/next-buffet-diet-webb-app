bdf7d1a47fa1ca2ee4f436c869c01fbf74937a8f
B3Kay <benjaminjekarlsson@gmail.com>
2024-09-24 14:29:32 +0200

    Update db to allow authenticated user to remove their own likes

---
+++ pb_migrations/1727180571_updated_likes.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("00vpwd6t9vz1uhf")
+
+  collection.deleteRule = "@request.auth.id != userId.id"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("00vpwd6t9vz1uhf")
+
+  collection.deleteRule = null
+
+  return dao.saveCollection(collection)
+})
