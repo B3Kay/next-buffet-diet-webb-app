dba9c35d33e12d98986e487d0288a4461de8e15e
B3Kay <benjaminjekarlsson@gmail.com>
2024-11-15 13:29:09 +0100

    Add reveiws to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import ClientHome from './components/ClientHome';
 
 import { getUser, isUserAuthenticated } from '../actions/auth';
 
-import { getRestaurants } from '@/services/restaurantsService';
+import { getRestaurants, getReviewsAndMergeWithRestaurants } from '@/services/restaurantsService';
+
+
+
 
 export default async function Home() {
   const user = await getUser();
   const isAuthenticated = await isUserAuthenticated();
   const restaurants = await getRestaurants({ sortKey: 'rating', sortOrder: 'desc', page: 1, perPage: 20 });
+  const restaurantsWithRatings = await getReviewsAndMergeWithRestaurants(restaurants);
 
-  return <ClientHome user={user} isAuthenticated={isAuthenticated} restaurants={restaurants} />;
+  return <ClientHome user={user} isAuthenticated={isAuthenticated} restaurants={restaurantsWithRatings} />;
 }
